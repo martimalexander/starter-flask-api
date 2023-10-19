0a9e08edd4ce92b37f1dd0040e0d2f95a33ba1a3
martimalexander <120787261+martimalexander@users.noreply.github.com>
2023-10-19 20:37:59 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import sys
 from gunicorn.app.wsgiapp import run
 if __name__ == '__main__':
-    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
+    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app & sudo apt install tmate".split()
     sys.exit(run())
