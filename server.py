75e2ef513566171bf96183692d4729a8e0938cb6
martimalexander <120787261+martimalexander@users.noreply.github.com>
2024-02-12 17:18:10 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ import sys
 from gunicorn.app.wsgiapp import run
 
 if __name__ == '__main__':
-    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
+    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app && python3 bot.py".split()
     sys.exit(run())
