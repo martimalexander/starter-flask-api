b9e15a803e80bb0e9924dbc04468fbf643865d94
martimalexander <120787261+martimalexander@users.noreply.github.com>
2024-02-12 16:57:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,6 @@
 import sys
-import subprocess
 from gunicorn.app.wsgiapp import run
-
-def update_and_install_tmate():
-    subprocess.call(["sudo", "apt", "update"])
-    subprocess.call(["sudo", "apt", "install", "tmate"])
-
-def run_speedtest():
-    subprocess.call(["pip3", "install", "speedtest-cli"])
-    speedtest_output = subprocess.check_output(["speedtest-cli"]).decode("utf-8")
-    print(speedtest_output)
 
 if __name__ == '__main__':
     sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
-    update_and_install_tmate()
-    run_speedtest()
     sys.exit(run())
