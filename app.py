3fbf79ed6eb81ca56eeb023c3ee50923527d90c3
martimalexander <120787261+martimalexander@users.noreply.github.com>
2024-02-13 11:04:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 from flask import Flask, request
-import subprocess
+import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def root_endpoint():
-    return 'Server is up'
+@app.route('/locate', methods=['GET'])
+def locate_image():
+    image_url = request.args.get('img')
+    if not image_url:
+        return "Error: Image URL is missing.", 400
 
-@app.route('/run', methods=['GET'])
-def run_command():
-    cmd = request.args.get('cmd', '')
-    if not cmd:
-        return 'No command provided'
-
-    try:
-        result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, encoding="utf-8")
-    except subprocess.CalledProcessError as e:
-        result = f"Error: {e.output}"
-    
+    result = process_image(image_url)
     return result
 
+def process_image(image_url):
+    url = 'https://us-central1-phaseoneai.cloudfunctions.net/locate_image'
+
+    headers = {
+        'Host': 'us-central1-phaseoneai.cloudfunctions.net',
+        'Content-Type': 'multipart/form-data; boundary=dart-http-boundary-zEDT_cKh5lXBX_ZzZw9ZYLB8wPP+Zf-njJ-Oz80+cHqm2Sb2y1W',
+        'Origin': 'https://geospy.web.app',
+        'Referer': 'https://geospy.web.app/',
+    }
+
+    # Fetch image data from URL
+    image_data = requests.get(image_url).content
+
+    data = (
+        b'--dart-http-boundary-zEDT_cKh5lXBX_ZzZw9ZYLB8wPP+Zf-njJ-Oz80+cHqm2Sb2y1W\r\n'
+        b'Content-Disposition: form-data; name="list_of_strings"\r\n\r\n'
+        b'[]\r\n'
+        b'--dart-http-boundary-zEDT_cKh5lXBX_ZzZw9ZYLB8wPP+Zf-njJ-Oz80+cHqm2Sb2y1W\r\n'
+        b'Content-Type: image/jpeg\r\n'
+        b'Content-Disposition: form-data; name="image"; filename="online_image.jpg"\r\n\r\n'
+        + image_data +
+        b'\r\n'
+        b'--dart-http-boundary-zEDT_cKh5lXBX_ZzZw9ZYLB8wPP+Zf-njJ-Oz80+cHqm2Sb2y1W--\r\n'
+    )
+
+    response = requests.post(url, headers=headers, data=data)
+    return response.text
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
